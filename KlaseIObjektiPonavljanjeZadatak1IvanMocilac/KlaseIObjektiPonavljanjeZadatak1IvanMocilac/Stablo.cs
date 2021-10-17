e9dd1300341ecb6bfd65efe404968fa8130cb3f0
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-10-17 11:30:33 +0200

    Add files via upload

---
+++ KlaseIObjektiPonavljanjeZadatak1IvanMocilac/KlaseIObjektiPonavljanjeZadatak1IvanMocilac/Stablo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseIObjektiPonavljanjeZadatak1IvanMocilac
+{
+    class Stablo : Biljka
+    {
+        bool opadajuListovi;
+
+        public bool OpadajuListovi
+        {
+            get => opadajuListovi;
+            set => opadajuListovi = value;
+        }
+
+        public Stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+    }
+}
